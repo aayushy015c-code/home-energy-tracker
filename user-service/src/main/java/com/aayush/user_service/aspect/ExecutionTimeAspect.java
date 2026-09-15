09ed69a152fe1855122979609de3ccc12b5112ef
Aayush <aayushy015c@gmail.com>
2026-09-15 15:06:49 +0530

    AOP functionality

--- user-service/src/main/java/com/aayush/user_service/aspect/ExecutionTimeAspect.java
+++ user-service/src/main/java/com/aayush/user_service/aspect/ExecutionTimeAspect.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
